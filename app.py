36fafabf09579e715c058b3fb817f67ed2912840
Manas Gupta <guptamanas657@gmail.com>
2026-07-22 16:51:33 +0530

    Setting Up streamlit

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from langchain.chains import create_history_aware_retriever,create_retrieval_cha
 from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_core.chat_history import BaseChatMessageHistory
 
+import streamlit as st
+
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -13,6 +15,18 @@ import os
 
 os.environ['HF_TOKEN']=os.getenv("HF_TOKEN")
 embeddings=HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+
+#set up streamlit
+st.title("Conversational RAG With PDF Uploadsand chat history")
+st.write("Upload a PDF file and start chatting with it!")
+
+#input the api_key
+api_key=st.text_input("Enter the api key", type="password")
+
+#check if groq api key is provided
+
+
+
 
 
 

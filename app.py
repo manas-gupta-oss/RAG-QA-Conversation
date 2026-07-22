7b172b83b99945789768d5d6efdcbd9a21b07970
Manas Gupta <guptamanas657@gmail.com>
2026-07-22 14:39:04 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,8 @@
+from langchain_groq import ChatGroq
+from langchain_chroma import Chroma
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_history_aware_retriever,create_retrieval_chain
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.chat_history import BaseChatMessageHistory
+
+

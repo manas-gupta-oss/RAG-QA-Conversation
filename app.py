caf2c00a0f4220759b3f7203b62ecf69782bebc3
Manas Gupta <guptamanas657@gmail.com>
2026-07-23 13:25:56 +0530

    feat: add conversational RAG chain with session-based chat history

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.document_loaders import PyPDFLoader
 
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_community.chat_message_histories import ChatMessageHistory
+
 from langchain_core.prompts import (
     ChatPromptTemplate,
     MessagesPlaceholder
@@ -138,7 +141,6 @@ if api_key:
         retriever = vectorstore.as_retriever()
 
 
-        # Prompt for reformulating question
         contextualize_q_system_prompt = (
             "Given a chat history and the latest user question "
             "which might reference context in the chat history, "
@@ -146,7 +148,6 @@ if api_key:
             "without the chat history. Do not answer the question, "
             "just reformulate it if needed and otherwise return it as it is."
         )
-
 
         # Create contextualization prompt
         contextualize_q_prompt = ChatPromptTemplate.from_messages(
@@ -161,7 +162,50 @@ if api_key:
             ]
         )
 
+        history_aware_retriever=create_history_aware_retriever(llm,retriever,contextualize_q_prompt)
+        #Answer question
+        system_prompt=(
+            "You are an assistant for question-answering tasks"
+            "Use the following pieces of retrieved context to answer"
+            "the question.If you dont know the answer, say that you"
+            "dont know.Use three sentences maximum and keep the "
+            "answer concise"
+            "/n/n"
+            "{context}"
 
+
+        )
+
+        qa_prompt=ChatPromptTemplate.from_messages(
+            [
+                ("system",system_prompt),
+                MessagesPlaceholder("chat_history"),
+                ("human","{input}"),
+
+
+            ]
+        )
+
+        question_answer_chain=create_stuff_documents_chain(llm,qa_prompt)
+        rag_chain=create_retrieval_chain(history_aware_retriever,question_answer_chain)
+
+        def get_session_history(session:str)->BaseChatMessageHistory:
+            if session_id not in st.session_state.store:
+                st.session_state.store[session_id]=ChatMessageHistory()
+            return st.session_state.store[session_id]
+
+        conversational_rag_chain=RunnableWithMessageHistory(
+            rag_chain,get_session_history,
+            input_messages_key="input",
+            history_messages_key="chathistory",
+            output_messages_key="answer"
+
+        )
+
+        user_input=st.text_input("Your Question:")
+        
+
+        
 
 
 
